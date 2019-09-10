4abf94c3df7b6d6ce70178d84d7074702d4504de
tedliu-TW <m0461006@g2.usc.edu.tw>
2019-09-10 17:44:02 +0800

    第 1 章，作業 2 - 連續訂票功能 num1 (輸入的訂幾張票)  如果是-1 就會停止訂票

--- src/main/java/com/train/Tester.java
+++ src/main/java/com/train/Tester.java
@@ -4,16 +4,27 @@ import java.util.Scanner;
 
 public class Tester {
     public static void main(String[] args) {
-        int num1,num2;
+        int num1=0,num2;  //num1 = ticked
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter number of tickets: ");
-        num1 = scanner.nextInt();
-        System.out.println("How many round-trip tickets: ");
-        num2 =scanner.nextInt();
-        System.out.println("Total tickets:" + num1 );
-        System.out.println("Round-Trip:" + num2 );
-        Ticket num = new Ticket(num1,num2);
-        System.out.println("Total:"+num.cal());
+        while (num1!=-1 ){
+            System.out.println("Please enter number of tickets: ");
+            num1 = scanner.nextInt();
+            if (num1==-1){ //ticked=-1 end the process
+                break;
+            }
+            System.out.println("How many round-trip tickets: ");
+            num2 =scanner.nextInt();
+            System.out.println("Total tickets:" + num1 );
+            System.out.println("Round-Trip:" + num2 );
+            Ticket num = new Ticket(num1,num2);
+            System.out.println("Total:"+num.cal());
+
+
+        }
+
+
+
+
 
 
 
--- src/main/kotlin/com/train1/TesterKotlin.kt
+++ src/main/kotlin/com/train1/TesterKotlin.kt
@@ -2,24 +2,24 @@ package com.train
 import  java.util.*
 
 fun main() {
+    var number1=0; //輸入的票數
     val scanner =Scanner(System.`in`)
-    println("Please enter number of tickets:")
-    var number1 = scanner.next()
-    println("How many round-trip tickets:")
-    var number2 = scanner.nextInt()
+    while (number1!=-1){
+        println("Please enter number of tickets:")
+        var number1 = scanner.next()
+        if(number1.toInt()==-1){   //當使用者想結束訂票時，只要購買張數輸入 -1 ，即結束程式
+            break;
+        }
+        println("How many round-trip tickets:")
+        var number2 = scanner.nextInt()
 
-    var stu = Ticket1(number1,number2)
+        var stu = Ticket1(number1,number2)
 
-    println("Total tickets:"+number1)
-    println("Round-trip:"+number2)
-    println("Total:"+stu.print())
+        println("Total tickets:"+number1)
+        println("Round-trip:"+number2)
+        println("Total:"+stu.print())
 
-
-
-
-
-
-
+    }
 }
 class Ticket1(var number1: String, var number2:Int){
     fun print(){
